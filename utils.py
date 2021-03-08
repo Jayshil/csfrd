90863e35fbed9d8bda510186a494d740636b831e
Jayshil <jayshilpatel147@gmail.com>
2021-03-08 15:06:09 +0530

    Added log Schechter function to the utils

--- utils.py
+++ utils.py
@@ -97,3 +97,30 @@ def schechter_mag(M, phi1, m1, alpha):
     ef = np.exp(-10**m2)
     xxy = ab*cd*ef
     return xxy
+
+def log_schechter(lum, lum1, phi1, alpha):
+    """
+    The Normalised logarithmic Schechter Function
+    ---------------------------------------------
+    Parameters:
+    -----------
+    lum : float, numpy.ndarray
+        luminosity range
+    phi1 : float
+        normalisation constant
+    lum1 : float
+        characteristic luminosity
+        the 'knee' of the function
+    alpha : float
+        the faint-end slope of power law
+    -----------
+    return
+    -----------
+    float, numpy.ndarray
+        number of galaxies in given bin
+    """
+    logg = np.log10(lum) - np.log10(lum1)
+    ab = np.log(10)*phi1
+    bc = 10**((alpha+1)*logg)
+    cd = np.exp(-10**logg)
+    return ab*bc*cd
