9f8924c3f94a668d0339b389d37fa1fa355654bc
Jayshil <jayshilpatel147@gmail.com>
2021-03-15 16:39:45 +0530

    Minor changes

--- data.py
+++ data.py
@@ -11,8 +11,8 @@ a = True
 
 while a:
     paper = input('Enter the name of the paper: ')
-    f1 = open(p2 + 'data_' + paper.replace(' ',''), 'w')
-    f1.write('#Redshift\t M* \t M*_err\t Phi* \t Phi*_err\t Alpha\t Alpha_err\n')
+    f1 = open(p2 + 'data_' + paper.replace(' ','') + '.dat', 'w')
+    f1.write('#Redshift\t M* \t M*_err\t Phi* \t Phi*_err\t Alpha\t Alpha_err + \t + NameOfPaper + \n')
     z = input('Enter redshift: ')
     z_err = input('Enter the error in redshift: ')
     mag = input('Enter charactersitic abs magnitude: ')
@@ -21,7 +21,7 @@ while a:
     phi_err = input('Enter error in phi: ')
     alp = input('Enter alpha: ')
     alp_err = input('Enter the error in alpha: ')
-    f1.write(z + '\t' + z_err + '\t' + mag + '\t' + mag_err + '\t' + phi + '\t' + phi_err + '\t' + alp + '\t' + alp_err + '\n')
+    f1.write(z + '\t' + z_err + '\t' + mag + '\t' + mag_err + '\t' + phi + '\t' + phi_err + '\t' + alp + '\t' + alp_err + '\t' + paper.replace(' ','') + '\n')
     f1.close()
     abc = input('Do you want to continue? (Y/n): ')
     if abc == 'Y':
