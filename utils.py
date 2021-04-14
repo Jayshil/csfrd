01cc996ea6ff00f4a0b02a3ccaf4bbad0d600e6b
Jayshil <jayshilpatel147@gmail.com>
2021-04-14 21:43:21 +0530

    Imported get_quantils frunction from exotoolbox

--- utils.py
+++ utils.py
@@ -402,3 +402,37 @@ def asymmetric_gausian_distribution(mean, std1, std2, size):
     d3 = np.concatenate((dn, up))
     np.random.shuffle(d3)
     return d3
+
+def get_quantiles(dist,alpha = 0.68, method = 'median'):
+    """
+    get_quantiles function
+    ----------------------
+    Imported from `exotoolbox`
+    by Nestor Espinoza
+    see, https://github.com/nespinoza/exotoolbox
+    ---------------------------------------------
+    DESCRIPTION
+        This function returns, in the default case, the parameter median and the error% 
+        credibility around it. This assumes you give a non-ordered 
+        distribution of parameters.
+    OUTPUTS
+        Median of the parameter,upper credibility bound, lower credibility bound
+    """
+    ordered_dist = dist[np.argsort(dist)]
+    param = 0.0
+    # Define the number of samples from posterior
+    nsamples = len(dist)
+    nsamples_at_each_side = int(nsamples*(alpha/2.)+1)
+    if(method == 'median'):
+       med_idx = 0
+       if(nsamples%2 == 0.0): # Number of points is even
+          med_idx_up = int(nsamples/2.)+1
+          med_idx_down = med_idx_up-1
+          param = (ordered_dist[med_idx_up]+ordered_dist[med_idx_down])/2.
+          return param,ordered_dist[med_idx_up+nsamples_at_each_side],\
+                 ordered_dist[med_idx_down-nsamples_at_each_side]
+       else:
+          med_idx = int(nsamples/2.)
+          param = ordered_dist[med_idx]
+          return param,ordered_dist[med_idx+nsamples_at_each_side],\
+                 ordered_dist[med_idx-nsamples_at_each_side]
