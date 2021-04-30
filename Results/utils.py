c877e4262001c4d0eddb1337245aac25a468c1b4
Jayshil <jayshilpatel147@gmail.com>
2021-04-30 15:33:02 +0530

    Added IR luminosity function

--- Results/utils.py
+++ Results/utils.py
@@ -193,6 +193,19 @@ def log_schechter(lum, lum1, phi1, alpha):
     cd = np.exp(-10**logg)
     return ab*bc*cd
 
+#---------------------------
+# LF for IR luminosities
+#---------------------------
+
+def sandage(lums9, alp9, phi9, sig9, lst9):
+    ab = 1 + (lums9/lst9)
+    bc = np.log10(ab)
+    cd = bc**2
+    ef = -cd/(2*sig9*sig9)
+    gh = np.exp(ef)
+    xy = (lums9/lst9)**(1-alp9)
+    return xy*phi9*gh
+
 #-----------------------------------------------------------
 #-------------- Calculating SFRD ---------------------------
 #-----------------------------------------------------------
